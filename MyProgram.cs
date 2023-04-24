97af00f6411166a37e0fefe912abac968b4af9d0
McLain Barrett <mclainbarrett@gmail.com>
2023-04-24 15:36:09 -0400

    Adding files

---
+++ MyProgram.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace MultiBoid {
+	public class MyProgram {
+		int fieldSize = 1000;
+		int boidCount = 100;
+		int threadCount = 1;
+
+		List<Vector> boidPos = new List<Vector>(); 
+		List<Vector> boidVel = new List<Vector>();
+		
+		Stopwatch stopwatch = new Stopwatch();
+
+
+		static void Main(string[] args) {
+			Console.WriteLine("Hello, World!");
+			var pg = new MyProgram();
+			pg.Start();
+		}
+
+		public void Start() {
+			Random rand = new Random();
+			for (int i = 0; i < boidCount; i++) {
+				boidPos.Add(new Vector(rand.Next(fieldSize), rand.Next(fieldSize)));
+				boidVel.Add(new Vector(rand.Next(50), rand.Next(50)));
+			}
+			stopwatch.Start();
+
+			TestBracket((200, 1600, 400), (0, 4, 1), 30);
+
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+
+		void TestBracket((int lB, int uB, int inc) BoidRange, (int lB, int uB, int inc) ThreadRange, int ticks) {
+			//(int, int, int) => lowerBound, upperBound (exclusive), increment
+
+			var data = "";
+			for (int i = BoidRange.lB; i < BoidRange.uB; i += BoidRange.inc)
+				data += "\t" + i + " Boids";
+
+			for (int j = ThreadRange.lB; j < ThreadRange.uB; j += ThreadRange.inc) {
+				data += "\n" + j + " Threads\t";
+				for (int i = BoidRange.lB; i < BoidRange.uB; i += BoidRange.inc) {
+					data += Math.Ceiling(Test(i, j, ticks)) + "\t";
+				}
+			}
+
+			//Write data to file
+			StreamWriter SW = new StreamWriter("MultiBoidData.txt");
+			SW.Write(data);
+			SW.Dispose();
+			Console.WriteLine("Saved data to path: " + Path.GetFullPath("MultiBoidData.txt"));
+
+			double Test(int BoidCount, int ThreadCount, int ticks) {
+				Console.WriteLine(String.Format(">{0} boids running on {1} threads.", BoidCount, ThreadCount));
+				//Initilize
+				boidCount = BoidCount;
+				threadCount = ThreadCount;
+				boidPos.Clear();
+				boidVel.Clear();
+				Random rand = new Random();
+				for (int i = 0; i < BoidCount; i++) {
+					boidPos.Add(new Vector(rand.Next(fieldSize), rand.Next(fieldSize)));
+					boidVel.Add(new Vector(rand.Next(50), rand.Next(50)));
+				}
+
+				//Main Test
+				var t0 = DateTime.Now;
+				for (int i = 0; i < ticks; i++)
+					Tick(0.03f);
+				var time = (DateTime.Now - t0).TotalMilliseconds / ticks;
+				Console.WriteLine(String.Format("This took {0} milliseconds.", Math.Ceiling(time)));
+				return time;
+			}
+		}
+
+		void Tick(float dT) {
+			var outVel = new List<Vector>(new Vector[boidVel.Count]);
+			int atIndex = 0;
+			int threadSize = threadCount >= 0 ? boidCount / (threadCount + 1) : 0;
+			List<Thread> workerThreads = new List<Thread>();
+
+			//Start worker threads
+			for (int i = 0; i < threadCount; i++) {
+				int a = atIndex, b = (atIndex + threadSize);
+				workerThreads.Add(new Thread(new ThreadStart(() => Phys(a, b))));
+				workerThreads[i].Name = "Worker thread " + i;
+				workerThreads[i].Start();
+				atIndex += threadSize;
+			}
+
+			//Main thread works too
+			Phys(atIndex, outVel.Count);
+
+			//Await all threads
+			foreach (var thread in workerThreads)
+				thread.Join();
+
+			boidVel = outVel;
+
+			for (int i = 0; i < boidPos.Count; i++) {
+				//Update position
+				boidPos[i] += boidVel[i] * dT;
+				boidPos[i] = new Vector(boidPos[i].x % (float)fieldSize, boidPos[i].y % (float)fieldSize);
+			}
+
+
+			void Phys(int start, int end) {
+				for (int i = start; i < end; i++) {
+					float C = 0.001f;
+					float S = 1f;
+					float A = 0.01f;
+					float B = 5f; //Barrier force
+					Vector myVel = new Vector(boidVel[i]);
+
+					for (int j = 0; j < boidPos.Count; j++) {
+						if (i == j)
+							continue;
+						Vector dir = (boidPos[j] - boidPos[i]);
+
+						myVel = myVel.Rotate((dir.Angle - myVel.Angle) * C * dT);
+
+						if (dir.Length <= 10)
+							myVel = myVel.Rotate((-dir.Angle - myVel.Angle) * S * dT / dir.Length);
+
+						myVel = myVel.Rotate((boidVel[j].Angle - myVel.Angle) * A * dT / dir.Length);
+					}
+
+					//Update velocity
+					outVel[i] = myVel;
+				}
+			}
+		}
+	}
+}
---
+++ Vector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Vector {
+	public float x = 0;
+	public float y = 0;
+	
+	public Vector(float x = 0, float y = 0) {
+		this.x = x; this.y = y;
+	}
+
+	public Vector(Vector vector) {
+		x = vector.x; y = vector.y;
+	}
+
+	public Vector((float bearing, float speed) vec) {
+		var d2r = Math.PI / 180;
+		x = (float)(vec.speed * Math.Cos(vec.bearing * d2r));
+		y = (float)(vec.speed * Math.Sin(vec.bearing * d2r));
+	}
+
+
+	public static Vector operator +(Vector a, Vector b) {
+		return new Vector(a.x + b.x, a.y + b.y);
+	}
+
+	public static Vector operator -(Vector a, Vector b) {
+		return new Vector(a.x - b.x, a.y - b.y);
+	}
+
+	public static Vector operator *(Vector a, float b) {
+		return new Vector(a.x * b, a.y * b);
+	}
+
+	public static Vector operator /(Vector a, float b) {
+		return new Vector(a.x / b, a.y / b);
+	}
+
+
+	public float SqrLength { get { return x * x + y * y;} }
+	public float Length { get { return (float)Math.Sqrt(SqrLength); } }
+	public float Angle { get { return (float)(Math.Atan2(y, x) * 180 / Math.PI); } }
+
+
+	public Vector Normalized { get { return new Vector(x, y) / ((Length != 0) ? Length : 1); } }
+	public Vector Rotate(float angle) {
+		double d2r = Math.PI / 180;
+		return new Vector((float)(x*Math.Cos(angle * d2r) - y*Math.Sin(angle * d2r)), 
+							(float)(x*Math.Sin(angle * d2r) + y * Math.Cos(angle * d2r)));
+	}
+
+
+	public override string ToString() { return String.Format("({0}, {1})", x, y); }
+}
